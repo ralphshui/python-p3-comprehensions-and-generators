af4191f9c52db10697a45c1f5a7b40b6b9627d6f
Ralph Shui <rskc.2112@gmail.com>
2023-07-25 09:24:07 -0400

    completing tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [sentence + '!' for sentence in sentence_list]
+    return exclamation_list
